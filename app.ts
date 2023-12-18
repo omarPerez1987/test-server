3347609c19612328350b8f59fcc0df8f9074b252
Omar Perez <omar1987perez2@gmail.com>
2023-12-18 11:47:18 +0100

    add: running server

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-const port = 3000;
+const port = 4000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
